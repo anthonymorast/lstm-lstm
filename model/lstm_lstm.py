4900ac3298f34b89a01267a7ab3f17ba48d243f8
Anthony Morast <anthonymorast33@hotmail.com>
2017-12-01 21:16:42 -0700

    got a working hybrid model with just the time series values

---
+++ model/lstm_lstm.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+from matplotlib import pyplot
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+from math import *
+from pandas import DataFrame
+
+from data_handler import * 
+
+
+def lstm_cell(lstm_size):
+	return tf.contrib.rnn.BasicLSTMCell(lstm_size)
+
+
+if __name__ == "__main__":
+	train_size = 1000
+	test_size = 2000
+
+	dh = DataHandler("../dailydata/forex/EURUSD.csv")
+
+	# Creates 2 new columns that are lagged by 1. These columns are
+	# the 'features'.
+	dh.timeSeriesToSupervised()
+	dh.tsdata.columns = ['DATE_LAG', 'TICKER_LAG', 'DATE', 'TICKER']
+
+	tsvalues = dh.tsdata.values
+
+	train = tsvalues[:train_size, :]						# used to train lstm
+	test = tsvalues[train_size:(train_size+test_size), :]	# get errors for error lstm
+	out = tsvalues[(train_size+test_size):, :]				# predict these values with both lstm's
+															# compare lstm_v rmse with lstm_v + lstm_e prediction rsme
+
+	outx, outy = out[:, 1], out[:, 3]
+
+	trainx, trainy = train[:, 1], train[:, 3]
+	testx, testy = test[:, 1], test[:, 3]
+
+	testy_dates = test[:, 2]
+
+	trainx = trainx.reshape(trainx.shape[0], 1)
+	testx = testx.reshape(testx.shape[0], 1)
+	outx = outx.reshape(outx.shape[0], 1)
+	trainx = trainx.reshape((trainx.shape[0], 1, trainx.shape[1]))
+	testx = testx.reshape((testx.shape[0], 1, testx.shape[1]))
+	outx = outx.reshape((outx.shape[0], 1, outx.shape[1]))
+	
+	lstm_v = Sequential()
+	lstm_v.add(LSTM(50, input_shape=(trainx.shape[1], trainx.shape[2])))
+	lstm_v.add(Dense(1))
+	lstm_v.compile(loss='mean_squared_error', optimizer='adam')
+	# history = lstm_v.fit(trainx, trainy, epochs=25, batch_size=100, validation_data=(testx, testy), verbose=2, shuffle=False)
+	print("\n\nTraining Series Model...")
+	history = lstm_v.fit(trainx, trainy, epochs=25, batch_size=100, verbose=2, shuffle=False)
+
+	# get error data and train error lstm
+	yhat = lstm_v.predict(testx)
+	error = testy-yhat[:, 0]
+	mse = sum(error**2) / len(yhat)
+
+	# error = output (y) for each input (series value)
+	e_trainx = testx
+	e_trainy = error
+	
+	lstm_e = Sequential()
+	lstm_e.add(LSTM(50, input_shape=(e_trainx.shape[1], e_trainx.shape[2])))
+	lstm_e.add(Dense(1))
+	lstm_e.compile(loss='mean_squared_error', optimizer='adam')
+	print("\n\nTraining Error Model...")
+	history_e = lstm_e.fit(e_trainx, e_trainy, epochs=25, batch_size = 100, verbose=2, shuffle=False)
+
+
+	# with both models trained, pass in out_x to each prediction
+	yhat_v = lstm_v.predict(outx)
+	yhat_e = lstm_e.predict(outx)
+
+	error_v = outy - yhat_v[:, 0]	# get error of just the series lstm
+	mse_v = sum(error_v**2) / len(error_v)
+
+	yhat_ve = yhat_v + yhat_e
+	error_ve = outy - yhat_ve[:, 0]
+	mse_ve = sum(error_ve**2) / len(error_ve)
+
+	for i in range(len(outx)):
+		print("eurusd:", outy[i], ", naive prediction:", yhat_v[i, 0], ", hybrid prediction:", yhat_ve[i,0])
+	print("\n\nSingle model MSE:", mse_v, "\nHybrid MSE:", mse_ve)
