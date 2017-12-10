7da81ebefee6eac72c1c9ecd177e50d3e103d2ca
Anthony Morast <anthonymorast33@hotmail.com>
2017-12-09 19:57:06 -0700

    added ga, lstm class, and arima stuff

--- model/lstm_lstm.py
+++ model/lstm_lstm.py
@@ -1,10 +1,8 @@
-import tensorflow as tf
 from matplotlib import pyplot
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
 from math import *
 from pandas import DataFrame
 import random
+from lstm import *
 
 from data_handler import * 
 
@@ -44,64 +42,55 @@ if __name__ == "__main__":
 
 	testy_dates = test[:, 2]
 
+	# vec->2d
+	trainy = trainy.reshape(trainy.shape[0], 1)
 	trainx = trainx.reshape(trainx.shape[0], 1)
 	testx = testx.reshape(testx.shape[0], 1)
 	outx = outx.reshape(outx.shape[0], 1)
 	trainx = trainx.reshape((trainx.shape[0], 1, trainx.shape[1]))
 	testx = testx.reshape((testx.shape[0], 1, testx.shape[1]))
 	outx = outx.reshape((outx.shape[0], 1, outx.shape[1]))
-	
-	lstm_v = Sequential()
-	lstm_v.add(LSTM(50, input_shape=(trainx.shape[1], trainx.shape[2]), return_sequences=True))
-	lstm_v.add(LSTM(50, return_sequences=True))
-	lstm_v.add(LSTM(50))
-	lstm_v.add(Dense(1))
-	lstm_v.compile(loss='mean_squared_error', optimizer='adam')
-	# history = lstm_v.fit(trainx, trainy, epochs=25, batch_size=100, validation_data=(testx, testy), verbose=2, shuffle=False)
-	print("\n\nTraining Series Model...")
-	history = lstm_v.fit(trainx, trainy, epochs=200, batch_size=100, verbose=2, shuffle=False)
+
+	base = MyLSTM(trainx.shape[1], 1, [250 for _ in range(10)], trainy.shape[1], epochs=100, batch_size=100)
+	print("\n\nTraining Base Model...")
+	base.train(trainx, trainy)
 
 	# get error data and train error lstm
-	yhat = lstm_v.predict(testx)
+	yhat = base.predict(testx)
 	error = testy-yhat[:, 0]
 	mse = sum(error**2) / len(yhat)
 
 	# error = output (y) for each input (series value)
 	e_trainx = testx
-	e_trainy = error
-	
-	lstm_e = Sequential()
-	lstm_e.add(LSTM(50, input_shape=(e_trainx.shape[1], e_trainx.shape[2]), return_sequences=True))
-	lstm_e.add(LSTM(50, return_sequences=True))
-	lstm_e.add(LSTM(50))
-	lstm_e.add(Dense(1))
-	lstm_e.compile(loss='mean_squared_error', optimizer='adam')
+	e_trainy = error.reshape(error.shape[0], 1)
+
+	error = MyLSTM(e_trainx.shape[1], 3, [50 for _ in range(10)], e_trainy.shape[1], epochs=150, batch_size=100)
 	print("\n\nTraining Error Model...")
-	history_e = lstm_e.fit(e_trainx, e_trainy, epochs=150, batch_size = 100, verbose=2, shuffle=False)
+	error.train(e_trainx, e_trainy)
 
 
 	# with both models trained, pass in out_x to each prediction
-	yhat_v = lstm_v.predict(outx)
-	yhat_e = lstm_e.predict(outx)
+	yhat_v = base.predict(outx)
+	yhat_e = error.predict(outx)
 
 	error_v = outy - yhat_v[:, 0]	# get error of just the series lstm
-	mse_v = sum(abs(error_v**2)) / len(error_v)
+	mse_v = sum(error_v**2) / len(error_v)
 
 	yhat_ve = yhat_v + yhat_e
 	error_ve = outy - yhat_ve[:, 0]
-	mse_ve = sum(abs(error_ve)) / len(error_ve)
+	mse_ve = sum(error_ve**2) / len(error_ve)
 
 	yhat_vr = yhat_v.copy()
 	for i in range(len(yhat_vr)):
 		yhat_vr[i] += random.uniform(-.2, .2)
 	error_vr = outy - yhat_vr[:, 0]
-	mse_vr = sum(abs(error_vr)) / len(error_vr)
+	mse_vr = sum(error_vr**2) / len(error_vr)
 
 	yhat_range = yhat_v.copy()
 	for i in range(len(yhat_range)):
 		yhat_range[i] += random.uniform(-ma, ma)
 	error_range = outy - yhat_range[:,0]
-	mse_range = sum(abs(error_range)) / len(error_range)
+	mse_range = sum(error_range**2) / len(error_range)
 
 	#for i in range(len(outx)):
 	#	print("eurusd:", outy[i], ", naive prediction:", yhat_v[i, 0], ", hybrid prediction:", yhat_ve[i,0])
