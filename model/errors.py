f7c0f79cab0b2e82b43cffa88806276a8fb78d8e
Morast <anthonymorast33@hotmail.com>
2018-01-17 12:06:16 -0700

    began on errors, quite since there is no sklearn here

---
+++ model/errors.py
@@ -0,0 +1,40 @@
+##########
+#
+# An implementation of popular error measurements.
+# Will be used to compare the 'goodness' of models.
+#
+# Assume below that n = number obs, y = actual values,
+# and hat(y) = predicted values.
+##########
+
+#####
+# Mean Error = sum_1^n(hat(y_i) - y_i)/n
+#####
+def MeanError(y, y_hat):
+	n = len(y)
+	if n != len(y_hat):
+		print('Size of predicted values and actual values are not equal')
+		return
+	return float(sum([y_hat[i] - y[i] for i in range(0, n)])) / float(n)
+
+
+#####
+# Mean Absolute Error = sum_1^n(abs(hat(y_i) - y_i))/n
+#####
+
+#####
+# Mean Squared Error = sum_1^n((hat(y_i) - y_i)^2)/n
+#####
+
+#####
+# Root-Mean-Square Error = sqrt(Mean Squared Error)
+#####
+
+#####
+# Mean Absolute Percentage Error = (100%/n) sum_1^n(abs((hat(y_i) - y_i)/y_i))
+#####
+
+#####
+# Symmetric Mean Absolute Percentage Error = (100%/n) sum_1^n(abs(hat(y_i) - y_i)/(abs(y_i) + abs(hat(y_i))))
+#####
+
